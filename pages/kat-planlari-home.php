796902b03c6652637858889523e51a1e78d04493
Selman TUNÇ <selmantunc@yandex.com>
2022-06-01 18:03:07 +0300

    fff

--- pages/kat-planlari-home.php
+++ pages/kat-planlari-home.php
@@ -61,14 +61,15 @@ function stnc_wp_floor_adminMenu_stnc_map_homepage()
       <div class="col-lg-2">
         <div class=" card-cover overflow-hidden rounded-5 shadow-lg stnc-card mx-auto" style="background-color: <?php echo $building->color ?> ;">
           <div class="d-flex flex-column   text-white text-center text-shadow-1">
-            <h2 class="pt-5  display-6 lh-1 fw-bold text-center " style="color:<?php echo $building->text_color ?>  ;  font-size: 100px;">1. </h2>
+            <h2 class="pt-5  display-6 lh-1 fw-bold text-center " style="color:<?php echo $building->text_color ?>  ;  font-size: 100px;"><?php echo $building->short_name ?>. </h2>
             <h3 class="pt-3 mt-3 lh-1 fw-bold text-center ">Bina </h3>
             <div class="d-flex list-unstyled mt-auto ">
-              <svg class="bd-placeholder-img rounded-circle mt-auto" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <svg class="bd-placeholder-img rounded-circle mt-auto" width="140" height="140" xmlns="http://www.w3.org/2000/svg" 
+              role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <rect width="100%" height="100%" fill="#fff"></rect>
-                <text x="10%" y="40%" fill="#7066D1" dy=".3em" font-size="3.5em" font-weight="bold">25</text>
-                <text x="60%" y="45%" fill="#7066D1" dy=".3em " font-size="1.5em"> ofis</text>
-                <text x="25%" y="75%" fill="#7066D1" dy=".3em" font-size="2em" font-weight="bold">BOS</text>
+                <text x="10%" y="40%" fill="<?php echo $building->text_color ?>" dy=".3em" font-size="3.5em" font-weight="bold">25</text>
+                <text x="60%" y="45%" fill="<?php echo $building->text_color ?>" dy=".3em " font-size="1.5em"> ofis</text>
+                <text x="25%" y="75%" fill="<?php echo $building->text_color ?>" dy=".3em" font-size="2em" font-weight="bold">BOS</text>
                 <!-- <text x="20%" y="55%" fill="#7066D1" dy=".3em" font-size="2em" font-weight="bold">DOLU</text> -->
               </svg>
             </div>
@@ -76,9 +77,9 @@ function stnc_wp_floor_adminMenu_stnc_map_homepage()
         </div>
         <div class="h-100 overflow-hidden rounded-5 mx-auto">
           <div class="media text-muted pt-3 mx-auto">
-            <strong style="font-size:25px" class="d-block text-gray-dark mx-auto text-center "><?php echo $building->name ?></strong>
+            <strong style="font-size:25px;color:<?php echo $building->text_color ?> " class="d-block text-gray-dark mx-auto text-center "><?php echo $building->name ?></strong>
             <span style="font-size:20px" class="d-block text-gray-dark  mx-auto text-center">Toplam: 85 ofis <br>
-              <span>250 Metrekare</span>
+              <!-- <span>250 Metrekare</span> -->
             </span>
             <br>
             <select class="form-select form-select-sm mx-auto" style="width: 130px;" aria-label=".form-select-sm example" onchange="javascript:handleSelect(this)">
